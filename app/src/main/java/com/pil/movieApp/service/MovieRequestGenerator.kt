bf26a9d34ef82a085b98fe2ca554f7798015607e
tomas.viader <tomasviader5@gmail.com>
2023-04-30 20:10:55 -0300

    Fixing request from api

--- app/src/main/java/com/pil/movieApp/service/MovieRequestGenerator.kt
+++ app/src/main/java/com/pil/movieApp/service/MovieRequestGenerator.kt
@@ -5,19 +5,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object MovieRequestGenerator {
-    private const val API_MOVIES_URL = "https://developers.themoviedb.org/"
+    private const val API_MOVIES_URL = "https://api.themoviedb.org/"
     private const val API = "apikey"
 
     private val httpClient = OkHttpClient.Builder().addInterceptor { chain ->
         val defaultRequest = chain.request()
         val defaultHttpUrl = defaultRequest.url()
         val httpUrl = defaultHttpUrl.newBuilder()
-            .addQueryParameter(API,"6d832682735f4138c76d2b1e20bb10e6")
+            .addQueryParameter(API,"api_key=6d832682735f4138c76d2b1e20bb10e6")
             .build()
         val requestBuilder = defaultRequest.newBuilder().url(httpUrl)
         chain.proceed(requestBuilder.build())
     }
-
 
 
     private val builder = Retrofit.Builder()
