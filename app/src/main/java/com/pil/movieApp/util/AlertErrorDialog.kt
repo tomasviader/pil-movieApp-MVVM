eda5b8569100f0a8de783465df51a9d8efd1cc07
tomas.viader <tomasviader5@gmail.com>
2023-05-08 14:57:52 -0300

    FIX Error Button

--- app/src/main/java/com/pil/movieApp/util/AlertErrorDialog.kt
+++ app/src/main/java/com/pil/movieApp/util/AlertErrorDialog.kt
@@ -1,10 +1,8 @@
 package com.pil.movieApp.util
 
 import android.content.Intent
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 import com.pil.movieApp.activity.MainActivity
 import com.pil.retrofit_room.R
 
@@ -19,7 +17,8 @@ object AlertErrorDialog {
             setCancelable(false)
             setNeutralButton("OK"
             ) { _, _ ->
-                Toast.makeText(activity,"To home page",Toast.LENGTH_LONG).show()
+                val intent = Intent(activity, MainActivity::class.java)
+                activity.startActivity(intent)
             }
         }
         alert.show()
